0d1903deb07866e962980ee05b214fe45358765a
beatrizcn7 <1201800@isep.ipp.pt>
2024-07-29 15:42:36 +0100

    + fotos e anotações

--- Ponto.py
+++ Ponto.py
@@ -54,13 +54,9 @@ def baixar_imagem(url, output_folder, image_name):
     except requests.exceptions.RequestException as e:
         print(f"Erro durante a requisição HTTP: {e}")
 
-url = ("https://www.google.com/maps/@39.4514533,-8.195438,3a,32.8y,229.66h,88.43t/data=!3m8!1e1!3m6!1sAF1QipNdCPagcVwErgXDwICa7ac_tV22RZJvKDgY3JYP!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNdCPagcVwErgXDwICa7ac_tV22RZJvKDgY3JYP%3Dw900-h600-k-no-pi1.5689901495379956-ya229.65717105156241-ro0-fo90!7i5376!8i2688?coh=205410&entry=ttu")
-
-
-
-
+url = ("https://www.google.com/maps/@38.9574803,-8.5231671,3a,75y,197.08h,89.51t/data=!3m8!1e1!3m6!1sAF1QipNwFK6a7kSQacDntcfpVS6x-3tUU9jXINMNilMY!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNwFK6a7kSQacDntcfpVS6x-3tUU9jXINMNilMY%3Dw900-h600-k-no-pi0.4902909982945971-ya244.08392550366034-ro0-fo90!7i10240!8i5120?coh=205410&entry=ttu")
 
 output_folder = "Fotos"
-image_name = "305_"
+image_name = ("574_1")
 
 baixar_imagem(url, output_folder, image_name)
