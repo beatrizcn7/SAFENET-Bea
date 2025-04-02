f905aab7742401ad02269b40695905df8ba95623
beatrizcn7 <1201800@isep.ipp.pt>
2025-04-02 13:43:23 +0100

    Comentado e Organizado

--- Material + Ano/ResNet/Tentativa 1/Do Zero/Material + Ano (ResNet) - Tentativa 1 Do Zero.py
+++ Material + Ano/ResNet/Tentativa 1/Do Zero/Material + Ano (ResNet) - Tentativa 1 Do Zero.py
@@ -1,3 +1,9 @@
+# Beatriz Neves, 30 de dezembro de 2024
+# Input = Pasta Final TFRecord - Material + Ano (Treino, Teste e Validação)
+# Output = Impressão da loss e accuracy no terminal assim como o tempo
+# Objetivo = Treinar um modelo ResNet50 com a informação input
+
+
 # ----------------- Bibliotecas ---------------
 # Importar a principal biblioteca de Machine Learning e Deep Learning.
 # Criar, treinar e implementar modelos de redes neurais.
